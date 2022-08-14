f82e8833ca39178c70a54e8cf2605bc20c732e79
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-08-14 15:38:50 -0300

    Added sequencias.py

---
+++ semestre-2/programacao-orientada-a-objetos/aula03/sequencias.py
@@ -0,0 +1,18 @@
+# listas
+inteiros = [1, 2, 3]
+
+strings = ['a', 'b', 'c']
+
+lista_mista = [1, False, 'abc', None]
+
+# tuplas
+inteiros = (1, 2, 3)
+
+strings = ('a', 'b', 'c')
+
+lista_mista = (1, False, 'abc', None)
+
+# intervalos (range)
+range(fim)
+range(inicio, fim)
+range(inicio, fim, passo)
