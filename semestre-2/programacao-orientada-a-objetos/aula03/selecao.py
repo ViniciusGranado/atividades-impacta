973131aa86ea410bb5d34ca634ead5e564df12c0
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-08-14 15:42:02 -0300

    Added selecao.py

---
+++ semestre-2/programacao-orientada-a-objetos/aula03/selecao.py
@@ -0,0 +1,10 @@
+n = int(input('Digite um número: '))
+
+if n > 10:
+    print('n é maior que 10')
+elif n == 10:
+    print('n é igual a 10')
+else:
+    print('n é menor ou igual a 10')
+
+print('fim')
