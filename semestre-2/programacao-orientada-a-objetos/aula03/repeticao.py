bd9ff27cba44728ae15983ec4702c21f00202397
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-08-14 15:45:18 -0300

    Added repeticao.py

---
+++ semestre-2/programacao-orientada-a-objetos/aula03/repeticao.py
@@ -0,0 +1,17 @@
+cont = 0
+while cont < 10:
+    print(cont)
+    cont += 1
+
+print('fim while')
+
+for cont2 in range(5):
+    print(cont2)
+
+print('fim for')
+
+n = int(input('Digite um número: '))
+for cont3 in range(n):
+    print(cont3)
+
+print('fim for range n')
