ef5c2773d3841290bf22328a1588ace3b1900130
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-08-14 15:52:11 -0300

    Added dicionarios.py

---
+++ semestre-2/programacao-orientada-a-objetos/aula03/dicionarios.py
@@ -0,0 +1,9 @@
+notas = {'Jack': 6.0, 'Anna': 9.0, 'Megan': 8.0}
+
+pessoa = {
+    'nome': 'Vinicius',
+    'cor favorita': 'laranja',
+    'idade': 27,
+    'telefone': '(11) 999-888-777',
+    'email': 'vinicius.r.granado@gmail.com'
+}
